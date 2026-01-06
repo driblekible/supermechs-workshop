a03135b73d9ac039431292b1cbce5a9282af6201
Drible Kible <151418209+driblekible@users.noreply.github.com>
2026-01-06 08:15:35 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import legacy from '@vitejs/plugin-legacy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
@@ -12,6 +11,7 @@ export default defineConfig({
   ],
   publicDir: 'static',
   build: {
-    outDir: 'public',
+    outDir: 'dist',
+    emptyOutDir: true
   },
 })
